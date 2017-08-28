370963f8fccadde838199751a70440fa28e2ad52
turchak <turchaksv@gmail.com>
2017-08-28 17:03:10 +0300

    deleted console.log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,15 @@
 let acc = document.querySelectorAll('.accordion');
-console.log(acc);
-acc.forEach((i) =>{
-    i.onclick = function (){
-      let next = this.nextElementSibling;
-      if (next.getAttribute("data-status", "opened")){
-        next.style.display = "none";
-        next.removeAttribute ("data-status", "opened");
+acc.forEach((i) => {
+  i.onclick = function() {
+    let next = this.nextElementSibling;
+    if (next.getAttribute("data-status", "opened")) {
+      next.style.display = "none";
+      next.removeAttribute("data-status", "opened");
 
-      } else {
+    } else {
       next.style.display = "block";
       next.setAttribute("data-status", "opened");
-      }
-    };
+    }
+  };
 
 });
